1080814f47231fadf08fbb91f9389c8a17318d83
Jesse <jesse.huang@zoom.us>
2020-07-21 14:56:54 +0800

    primes

--- user/primes.c
+++ user/primes.c
@@ -6,31 +6,63 @@ int main(int argc, char *argv[])
 {
     int fd[2];
     pipe(fd);
-    int pid = fork();
-    if (pid > 0)
+    close(fd[0]);
+    for (int i = 2; i <= 35; i++)
     {
-        printf("prime %d\n", 2);
-        close(fd[0]);
-        //parent process
-        for (int i = 3; i <= 35; i++)
-        {
-            if (i % 2 != 0)
-            {
-                printf("prepare to write:%d\n", i);
-                write(fd[1], &i, 4);
-            }
-        }
-        close(fd[0]);
+        write(fd[1], &i, 4);
     }
-    else if (pid == 0)
-    {
-        //child process
-        close(fd[1]);
-        int res;
-        while (read(fd[0], &res, 4) > 0)
-        {
-            printf("read process %d\n", res);
-        }
-    }
+    handle_data(fd);
     exit();
+}
+
+/**
+ * handel data in pipeline
+ */
+void handle_data(int left_fd[2])
+{
+    close(left_fd[1]); // close write end
+    int prime;
+    int prime_read_res=read(left_fd[0], &prime, 4);
+    if(prime_read_res<0){
+        return;
+    }
+    printf("prime %d", prime);
+
+    int data;
+    int read_res = read(left_fd[0], &data, 4);
+    if (read_res > 0)
+    {
+        // has more data
+        // fork a process to handle next prime
+        int right_fd[2];
+        pipe(right_fd);
+
+        int pid = fork();
+        if (pid > 0)
+        {
+            close(right_fd[0]); // close read end
+            //parent
+            do
+            {
+                if (data % prime != 0)
+                {
+                    //write to next process
+                    write(right_fd[1], &data, 4);
+                }
+            } while (read_res = read(left_fd[0], &data, 4) > 0);
+        }
+        else if (pid == 0)
+        {
+            //child
+            process(right_fd);
+        }
+    }
+    else if (read_res == 0)
+    {
+        // no data, do nothing
+    }
+    else
+    {
+        printf("read data from pipe error!");
+    }
 }
