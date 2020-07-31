7104d9ed168c0a766b5de4c4e1dda13fb3dd9bd9
Jesse <jesse.huang@zoom.us>
2020-07-31 08:33:55 +0800

    add param to params

--- user/xargs.c
+++ user/xargs.c
@@ -22,9 +22,11 @@ int main(int argc, char *argv[])
 
                 //fork and execute command
                 char *params[MAXARG];
+                int paramIndex = 0;
                 for (int i = 1; i < sizeof(argv); i++)
                 {
                     printf("%d param is %s\n", i, argv[i]);
+                    params[paramIndex++] = argv[i];
                 }
 
                 i = 0;
